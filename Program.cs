67c6543a924fcdc37f366c8417ff85dc56e5618f
alexk <alexk@DESKTOP-QL7PNT4>
2024-05-23 12:49:28 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Assignment_3._2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] firstMatrix = { { 1, 2 }, { 3, 4 } };
+            int[,] secondMatrix = { { 5,6 }, { 7, 8 } };
+
+            int[,] addMaxtrix = new int[2, 2];
+            int firstMatrixColumn = 0;
+            int secondMatrixColumn = 0;
+            for (int i =0; i <firstMatrix.GetLength(0); i++) 
+            {
+                addMaxtrix[firstMatrixColumn,i] = firstMatrix[firstMatrixColumn,i] + secondMatrix[secondMatrixColumn,i];   
+            }
+            firstMatrixColumn = 1;
+            secondMatrixColumn = 1;
+
+            for (int i = 0; i < firstMatrix.GetLength(0); i++) 
+            {
+                addMaxtrix[firstMatrixColumn, i] = firstMatrix[firstMatrixColumn, i] + secondMatrix[secondMatrixColumn, i];
+            }
+           
+            foreach (int i in addMaxtrix) 
+            {
+                Console.WriteLine(i);   
+            }
+        }
+    }
+}
